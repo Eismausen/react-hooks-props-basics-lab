fcef4c1e8a6dfc7d0395447a77e7ff5f5cf7d845
David Avery <eismausen@gmail.com>
2021-11-03 10:53:30 -0700

    commit tree 9382253bd86ba62ac9b1499f9db6b2425c9d9afd

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About({bio='', linkedin, github}) {
+  const bioCheck = bio;
+  if (bio !== '') {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links linkedin={linkedin} github={github} />
+      </div>
+    )} else {
+      return (
+        <div id='about'>
+          <h2>About Me</h2>
+          <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+          <Links linkedin={linkedin} github={github} />
+        </div>
+      )
+    }
 }
 
 export default About;
